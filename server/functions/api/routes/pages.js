44224bfea03a35d99143e95d90e4fced93457473
singharaj usai <djjd22938@gmail.com>
2024-09-16 00:50:50 -0400

    pages fix

--- server/functions/api/routes/pages.js
+++ server/functions/api/routes/pages.js
@@ -7,30 +7,30 @@ const messagesRoutes = require('./messages'); // Add this line
 const router = express.Router();
 
 router.get("/navbar", (req, res) => {
-  res.sendFile(path.join(__dirname, "../../../../client/views/navbar.html"));
+  res.sendFile(path.join(__dirname, "../../../../client/navbar.html"));
 });
 
 router.get("/login", isAuthenticated, (req, res) => {
-  res.sendFile(path.join(__dirname, "../../../../client/views/login.html"));
+  res.sendFile(path.join(__dirname, "../../../../client/login.html"));
 });
 
 router.get("/register", isAuthenticated, (req, res) => {
-  res.sendFile(path.join(__dirname, "../../../../client/views/register.html"));
+  res.sendFile(path.join(__dirname, "../../../../client/register.html"));
 });
 
 router.get("/search-results", (req, res) => {
-  res.sendFile(path.join(__dirname, "../../../../client/views/search-results.html"));
+  res.sendFile(path.join(__dirname, "../../../../client/search-results.html"));
 });
 
 router.get("/user-profile", (req, res) => {
-  res.sendFile(path.join(__dirname, "../../../../client/views/user-profile.html"));
+  res.sendFile(path.join(__dirname, "../../../../client/user-profile.html"));
 });
 
 router.use("/api/messages", messagesRoutes); // Add this middleware
 
 // Route to serve the "My Messages" page
 router.get("/messages", (req, res) => {
-  res.sendFile(path.join(__dirname, "../../../../client/views/messages.html"));
+  res.sendFile(path.join(__dirname, "../../../../client/my/messages.html"));
 });
 
 router.get("/messages/compose", (req, res) => {
