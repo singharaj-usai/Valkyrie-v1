abb1ace776832613355b077e976e7350d71a505a
SevenRBX <46130959+SevenRBX@users.noreply.github.com>
2024-09-17 19:53:34 +0100

    Update emailService.js
    Maybe this fixed it

--- server/functions/api/utils/emailService.js
+++ server/functions/api/utils/emailService.js
@@ -1,13 +1,11 @@
 const nodemailer = require('nodemailer');
 const { google } = require('googleapis');
 
-const OAuth2 = google.auth.OAuth2;
-
 const createTransporter = async () => {
   const oauth2Client = new OAuth2(
     process.env.GOOGLE_CLIENT_ID,
     process.env.GOOGLE_CLIENT_SECRET,
-    "https://developers.google.com/oauthplayground"
+    "https://accounts.google.com/o/oauth2/auth"
   );
 
   oauth2Client.setCredentials({
@@ -24,7 +22,10 @@ const createTransporter = async () => {
   });
 
   const transporter = nodemailer.createTransport({
-    service: "gmail",
+    //service: "gmail",
+    host: 'smtp.gmail.com',
+    port: 465,
+    secure: true,
     auth: {
       type: "OAuth2",
       user: process.env.EMAIL_USERNAME,
