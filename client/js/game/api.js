7753f758bbf96d929db7c4c68ed731bf11de4b0e
singharaj usai <djjd22938@gmail.com>
2024-09-25 02:50:31 -0400

    client game.js folder

---
+++ client/js/game/api.js
@@ -0,0 +1,18 @@
+const GameAPI = {
+    fetchGameDetails: function(gameId) {
+        const token = localStorage.getItem('token');
+        $.ajax({
+            url: `/api/games/${gameId}`,
+            method: 'GET',
+            headers: {
+                'Authorization': `Bearer ${token}`
+            },
+            success: function (game) {
+                GameDisplay.displayGameDetails(game);
+            },
+            error: function (xhr, status, error) {
+                GameUtils.showError('Error fetching game details: ' + (xhr.responseJSON ? xhr.responseJSON.error : 'Unknown error'));
+            }
+        });
+    }
+};
