279e83b2b0bc411d46f8aa677f625696ba932c48
singharaj usai <djjd22938@gmail.com>
2024-09-25 02:07:21 -0400

    friends folder organization

---
+++ client/js/friends/actions.js
@@ -0,0 +1,32 @@
+$(document).on('click', '.accept-request', function() {
+    const userId = $(this).data('id');
+    sendFriendAction('/api/accept-friend-request/' + userId, 'Friend request accepted');
+});
+
+$(document).on('click', '.decline-request', function() {
+    const userId = $(this).data('id');
+    sendFriendAction('/api/decline-friend-request/' + userId, 'Friend request declined');
+});
+
+$(document).on('click', '.unfriend', function() {
+    const userId = $(this).data('id');
+    sendFriendAction('/api/unfriend/' + userId, 'Unfriended successfully');
+});
+
+function sendFriendAction(url, successMessage) {
+    $.ajax({
+        url: url,
+        method: 'POST',
+        headers: {
+            "Authorization": `Bearer ${token}`
+        },
+        success: function(response) {
+            alert(successMessage);
+            fetchFriendRequests();
+            fetchFriendsList();
+        },
+        error: function(xhr, status, error) {
+            alert('Error: ' + (xhr.responseJSON ? xhr.responseJSON.error : 'Unknown error'));
+        }
+    });
+}
