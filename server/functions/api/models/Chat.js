5cd1e36d210ffb2ee68033af50db64986bc6e04f
singharaj usai <djjd22938@gmail.com>
2024-10-04 15:33:32 -0400

    chat message schemas

---
+++ server/functions/api/models/Chat.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+
+const chatSchema = new mongoose.Schema({
+  sender: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  recipient: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  message: {
+    type: String,
+    required: true
+  },
+  timestamp: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = mongoose.model('Chat', chatSchema);
