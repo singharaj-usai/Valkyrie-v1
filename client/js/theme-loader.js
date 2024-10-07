1cfb1f058e20c493ed5794019246686926eed626
singharaj usai <djjd22938@gmail.com>
2024-10-07 18:08:33 -0400

    cosmo metro theme theme

--- client/js/theme-loader.js
+++ client/js/theme-loader.js
@@ -4,14 +4,14 @@
         const themeLink = document.getElementById('theme-stylesheet');
         themeLink.href = `https://cdnjs.cloudflare.com/ajax/libs/bootswatch/3.3.7/${theme}/bootstrap.min.css`;
         
-        let submenuTop = '65px';
+        let submenuTop = '50px';
         if (theme === 'cyborg') {
             submenuTop = '50px';
         }
         document.documentElement.style.setProperty('--user-submenu-top', submenuTop);
     }
 
-    const theme = localStorage.getItem('theme') || 'paper';
+    const theme = localStorage.getItem('theme') || 'cosmo';
     applyTheme(theme);
 
     const style = document.createElement('style');
--- server/functions/api/routes/pages.js
+++ server/functions/api/routes/pages.js
@@ -22,8 +22,8 @@ router.get("/auth/verify-email/:token", (req, res) => sendHtmlFile(res, "pages/a
 
 // User-related pages
 router.get("/users", (req, res) => sendHtmlFile(res, "pages/users/users.html"));
-//router.get("/user-profile", (req, res) => sendHtmlFile(res, "pages/profile/user-profile.html"));
-router.get("/users/:userId/profile", (req, res) => sendHtmlFile(res, "pages/profile/user-profile.html"));
+router.get("/user-profile", (req, res) => sendHtmlFile(res, "pages/profile/user-profile.html"));
+//router.get("/users/:userId/profile", (req, res) => sendHtmlFile(res, "pages/profile/user-profile.html"));
 
 
 // Forum pages
