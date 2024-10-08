afcecb98d08dc8692730ae54bbb796158c08f113
singharaj usai <djjd22938@gmail.com>
2024-10-08 19:30:16 -0400

    testing google adsense ad banners

--- client/js/ads.js
+++ client/js/ads.js
@@ -1,8 +1,21 @@
 const Ads = {
     loadTopBanner: function() {
-        // This is a placeholder function. You'll need to replace this
-        // with actual ad loading code from your ad provider.
-        const adContent = '<div style="background-color: #f0f0f0; padding: 10px; text-align: center;">Advertisement Banner</div>';
-        $('#ad-banner-top').html(adContent);
+        const adScript = document.createElement('script');
+        adScript.async = true;
+        adScript.src = 'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-YOUR_PUBLISHER_ID';
+        adScript.crossOrigin = 'anonymous';
+        document.head.appendChild(adScript);
+
+        const adElement = document.createElement('ins');
+        adElement.className = 'adsbygoogle';
+        adElement.style.display = 'block';
+        adElement.setAttribute('data-ad-client', 'ca-pub-6563673001934226');
+        adElement.setAttribute('data-ad-slot', '8580573535');
+        adElement.setAttribute('data-ad-format', 'auto');
+        adElement.setAttribute('data-full-width-responsive', 'true');
+
+        $('#ad-banner-top').html('').append(adElement);
+
+        (adsbygoogle = window.adsbygoogle || []).push({});
     }
 };
