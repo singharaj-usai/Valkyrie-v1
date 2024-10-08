774f0a1d9b8d0b2aede2f1c1345be7f081ea9a68
singharaj usai <djjd22938@gmail.com>
2024-10-08 16:50:58 -0400

    moved submenu a little lower 55px

--- client/js/theme-loader.js
+++ client/js/theme-loader.js
@@ -4,7 +4,7 @@
         const themeLink = document.getElementById('theme-stylesheet');
         themeLink.href = `https://cdnjs.cloudflare.com/ajax/libs/bootswatch/3.3.7/${theme}/bootstrap.min.css`;
         
-        let submenuTop = '50px';
+        let submenuTop = '55px';
         if (theme === 'cyborg') {
             submenuTop = '50px';
         }
