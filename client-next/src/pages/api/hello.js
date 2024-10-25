d401163e4dc26be457329b1bf74ad8f02ba3e563
singharaj usai <djjd22938@gmail.com>
2024-10-24 20:27:36 -0400

    vercel.json

--- client-next/src/pages/api/hello.js
+++
@@ -1,7 +0,0 @@
-export default function handler(req, res) {
-  try {
-    res.status(200).json({ message: 'Hello from API!' });
-  } catch (error) {
-    res.status(500).json({ error: 'Internal Server Error' });
-  }
-}
